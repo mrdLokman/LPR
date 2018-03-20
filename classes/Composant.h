2fa5013e88090e1e74e23b93501484af56f7f3cd
mrdLokman <merdjilokman@gmail.com>
2018-03-20 09:30:10 +0100

    Update Composant.h

--- classes/Composant.h
+++ classes/Composant.h
@@ -17,28 +17,6 @@ public:
 
 	Mat data;
 	map<string, double> attributs;
-	vector<vector<cv::Point>>contours;
-	//vector<Vec4i> hierarchie;
-	vector<cv::Point>contour_externe;
-	//***********
-	//features
-	//***********
-	double largeur;
-	double hauteur;
-	double ratio;
-	int ordreHorizontal;
-	double densite;
-	//contour-feature
-	int nbr_contours;
-	double surface;
-	double perimeter;
-	vector<double> contour_aproximation_scores;
-	//geometrie features
-	Point2f centre_de_masse;
-	int crossings;
-	cv::Moments moments;
-	double dtw;
-	//*******************************************************methodes()
 	
 	int ordreHorizontal;
 	double debutX;
@@ -53,21 +31,7 @@ public:
 	double ratio();
 	double densite();
 	double portionHauteur();
-	void setData(cv::Mat src);
-	void setContourExterne();
-	void setPositionX(int);
-	void setPositionY(int);
-	void setRatio();
-	void setOrdreHorizontal(int);
-	void setDensite();
-	void setNbrContours();
-	void setSurface();
-	void setPerimeter();
-	void setContourAproximationScores();
-	void setCentreDeMasse();
-	void setCrossings();
-	void setMoments();
-	void setDTW();//
+	
 
 
 };
